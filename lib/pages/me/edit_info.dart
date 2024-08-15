7d42fcd688f7a90f4e051b9f3df7f863b55e8a2d
coral <coralyang@dh-data.com>
2024-08-15 16:31:30 +0800

    feat:编辑个人信息

--- lib/pages/me/edit_info.dart
+++ lib/pages/me/edit_info.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:iconify_flutter/icons/cil.dart';
 import 'package:flutter/material.dart';
 import 'package:food/widgets/header.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditInfo extends StatefulWidget {
   const EditInfo({super.key});
@@ -9,23 +14,76 @@ class EditInfo extends StatefulWidget {
 }
 
 class _EditInfoState extends State<EditInfo> {
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
           child: Padding(
-        padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+        padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
         child: Column(
           children: [
-            Header(title: '个人信息'),
-            SizedBox(
+            const Header(title: '个人信息'),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                const Text(
+                  '头像',
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(
+                  width: 25,
+                ),
+                InkWell(
+                    onTap: _pickImage,
+                    child: _image == null
+                        ? DottedBorder(
+                            borderType: BorderType.RRect,
+                            radius: const Radius.circular(4),
+                            padding: const EdgeInsets.all(30),
+                            child: const Iconify(
+                              Cil.plus,
+                              size: 40,
+                            ))
+                        : Image.file(
+                            _image!,
+                            width: 100,
+                            height: 100,
+                            fit: BoxFit.cover,
+                          ))
+              ],
+            ),
+            const SizedBox(
               height: 15,
             ),
-            TextField(
+            const TextField(
               obscureText: true,
               decoration: InputDecoration(
                   border: OutlineInputBorder(), labelText: '用户名'),
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: FilledButton(onPressed: () {}, child: const Text('提交')),
+            )
           ],
         ),
       )),
